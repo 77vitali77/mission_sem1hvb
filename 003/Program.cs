f011e2b4606ffd3bc7c7ae39e60375a4caa819f6
vitali <xudyakovvb@email.com>
2022-09-17 16:12:57 +0700

    задача3

---
+++ 003/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 1)
+{
+    Console.WriteLine("нечетное число, не делится на 2");
+}
+
+else
+{
+   Console.WriteLine("четное число, делится на 2");
+}
+
