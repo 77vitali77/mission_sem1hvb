bc763be122e578fcb352a33f4ed9562c3b112d43
vitali <xudyakovvb@email.com>
2022-09-28 20:53:26 +0700

    023

---
+++ 023/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.Write("Введите число N: ");   // Вводим число N
+int cube = Convert.ToInt32(Console.ReadLine());   //конвертируем и присваеваем
+
+void Cube(int[] cube)   //метод возведения в куб
+{
+  int length = cube.Length;
+  for (int counter = 0; counter <  length; counter++)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));    
+  }
+}
+
+void PrintArry(int[] coll)   //метод печати массива в консоль
+{
+  int count = coll.Length;  
+  for (int index = 0; index < count; index++)
+  {
+    Console.Write(coll[index]+ " ");    
+  }
+} 
+
+int[] arry = new int[cube+1];   //заплняем массив через Вводим число N
+Cube(arry);   //возводим в куб массив
+PrintArry(arry);   // печать массива в консоль
