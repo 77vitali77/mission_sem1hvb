cbdf7fcb01dffd24b9128e9176710b96fda40395
vitali <xudyakovvb@email.com>
2022-10-02 21:26:58 +0700

    029

---
+++ 029/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов 
+//и выводит их на экран.
+
+int[] array = {3,2,1,6,4,5,8,7};
+
+void printArray(int[] array)
+{
+  int count = array.Length;
+  for (int i = 0; i < count; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+printArray(array);
