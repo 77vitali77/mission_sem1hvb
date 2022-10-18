d15a1caea72fae2a3d236cb82ef2796d02debaba
vitali <xudyakovvb@email.com>
2022-10-18 09:03:24 +0700

    064

---
+++ 064/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 63: Задайте значение вещественное N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.Выполнить с помощью рекурсии
+
+
+double GetNumberFromConsole()
+ {
+     return Convert.ToDouble(Console.ReadLine());
+ }
+ 
+
+int a = 1;
+string WriteNumbersRecursion(double lastNumber, int a)
+ {
+     if (lastNumber >= a)
+        return lastNumber + ", " + WriteNumbersRecursion(lastNumber-1, a);
+    
+    return string.Empty;
+ }
+
+
+
+double number = GetNumberFromConsole();
+
+Console.Write(WriteNumbersRecursion(number,a));
+
